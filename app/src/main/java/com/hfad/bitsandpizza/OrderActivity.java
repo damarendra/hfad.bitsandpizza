0883a89408633e3be8a0b3d53abf6781f95dff9e
damarendra <damarendran@gmail.com>
2021-02-24 17:40:19 +0700

    Added Floating Action Button and Snackbar with Toast

--- app/src/main/java/com/hfad/bitsandpizza/OrderActivity.java
+++ app/src/main/java/com/hfad/bitsandpizza/OrderActivity.java
@@ -2,8 +2,14 @@ package com.hfad.bitsandpizza;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -13,5 +19,19 @@ public class OrderActivity extends AppCompatActivity {
         setContentView(R.layout.activity_order);
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void onClick(View view) {
+        CharSequence text = "Hello, I'am a Snackbar";
+        int duration = Snackbar.LENGTH_SHORT;
+        Snackbar snackbar = Snackbar.make(findViewById(R.id.container_order_activity), text, duration);
+        snackbar.setAction("Undo", new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(OrderActivity.this, "Undone!", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+        snackbar.show();
     }
 }
